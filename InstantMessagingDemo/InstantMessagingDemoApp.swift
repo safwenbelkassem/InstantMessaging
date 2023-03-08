efd2ac5c0459f770c66f91e714766c601fed77eb
Safwen <safouane.belkacem@satoripop.com>
2023-03-08 17:00:12 +0100

    Initial project setup

---
+++ InstantMessagingDemo/InstantMessagingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantMessagingDemoApp.swift
+//  InstantMessagingDemo
+//
+//  Created by Safouane Belkacem on 8/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstantMessagingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
