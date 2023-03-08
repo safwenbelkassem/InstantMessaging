91265dcae17a8c665e553dc0e47d30502f645a3c
Safwen <safouane.belkacem@satoripop.com>
2023-03-08 16:55:33 +0100

    Initial Commit

---
+++ InstantMessaging/InstantMessaging.h
@@ -0,0 +1,18 @@
+//
+//  InstantMessaging.h
+//  InstantMessaging
+//
+//  Created by Safouane Belkacem on 8/3/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstantMessaging.
+FOUNDATION_EXPORT double InstantMessagingVersionNumber;
+
+//! Project version string for InstantMessaging.
+FOUNDATION_EXPORT const unsigned char InstantMessagingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstantMessaging/PublicHeader.h>
+
+
